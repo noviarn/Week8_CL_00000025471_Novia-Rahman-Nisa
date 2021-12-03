ebb3bcd9b9da6131341ea96f677a6983f61960a2
noviarn <42713448+noviarn@users.noreply.github.com>
2021-12-03 18:46:29 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'week8_mobile',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
